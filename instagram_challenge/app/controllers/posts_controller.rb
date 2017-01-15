fd8e56ea4b9e2c376549fbbaf3c52418d4150e44
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-15 17:50:37 +0000

    Can create new post name and caption

--- instagram_challenge/app/controllers/posts_controller.rb
+++ instagram_challenge/app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    p post_params
+    Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :caption)
+  end
+
 end
--- instagram_challenge/spec/features/posts_feature_spec.rb
+++ instagram_challenge/spec/features/posts_feature_spec.rb
@@ -9,16 +9,27 @@ feature 'posts' do
     end
   end
 
-  context 'posts have been added' do
+  context 'when posts have been added' do
     before do
       Post.create(name: 'My Cat Mr. Paws')
     end
 
-    scenario 'display posts' do
+    scenario 'it will display posts' do
       visit '/posts'
       expect(page).to have_content('My Cat Mr. Paws')
       expect(page).not_to have_content('You have no posts yet')
     end
+
+    scenario 'CREATE: it prompts user to add an image, then displays the new picture' do
+        visit '/posts'
+        click_link 'Add a new photo or video'
+        fill_in 'Name', with: 'My Cat Mr. Paws'
+        fill_in 'Caption', with: 'Adorable Mr. Paws eats soup'
+        click_button 'Upload Image'
+        expect(page).to have_content 'My Cat Mr. Paws'
+        expect(current_path).to eq '/posts'
+      end
+
   end
 
 
