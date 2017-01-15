ba39362c817a84b404b4823d04fcad4a94dc561b
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-15 17:13:47 +0000

    Posts can be viewed on index

--- instagram_challenge/spec/features/posts_feature_spec.rb
+++ instagram_challenge/spec/features/posts_feature_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 feature 'posts' do
-  context 'no posts have been added' do
-    scenario 'should display a prompt to add a post' do
+  context 'when no posts have been added' do
+    scenario 'display a prompt to add a post' do
       visit '/posts'
       expect(page).to have_content 'You have no posts yet'
       expect(page).to have_link 'Add a new photo or video'
     end
   end
 
+  context 'posts have been added' do
+    before do
+      Post.create(name: 'My Cat Mr. Paws')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('My Cat Mr. Paws')
+      expect(page).not_to have_content('You have no posts yet')
+    end
+  end
 
 
 end
